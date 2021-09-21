5f33989816c0162c2df6b6c0102c4b1a6b8a1aab
Stefan <stefan.schlombs@gmail.com>
2021-09-21 23:08:37 +0200

    Event Update

--- Googlematerialicons.event.php
+++ Googlematerialicons.event.php
@@ -6,6 +6,9 @@ class PluginGooglematerialicons_event extends Base
     {
         // https://fonts.google.com/icons
 
+        // <CMS function=""></CMS>
+        // <span class="material-icons-outlined">favorite</span>
+
         ContainerExtensionTemplateParseInsertPositions::insert('/ContainerIndexPage/Template/Positions/Head',
                                                                '<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
       rel="stylesheet">');
@@ -13,6 +16,29 @@ class PluginGooglematerialicons_event extends Base
 
     public static function setHeader($triggering, $object, &$scope): void
     {
-        $scope['headerCMS']['style-src'][] = 'https://fonts.googleapis.com/icon?family=Material+Icons';
+        $scope['headerCMS']['style-src'][] = 'https://fonts.googleapis.com';
+        $scope['headerCMS']['font-src'][] = 'https://fonts.gstatic.com';
+    }
+
+    public static function setTemplateFunction($triggering, $object, &$scope): void
+    {
+        $template->setRegisteredFunctions('historyEditFormFooter',
+            function ($content, $htmlTags, $templateObject) {
+                /** @var ContainerExtensionTemplate $templateObject */
+
+                if ($templateObject->getAssign('APP_SOURCE') === ApplicationAdministrationContentEdit_app::CONTENT_SOURCE_MAIN) {
+
+                    /** @var ContainerExtensionTemplate $template */
+                    $template = Container::get('ContainerExtensionTemplate');
+                    $template->set($content);
+                    $template->parse();
+
+                    return $template->get();
+                }
+                else {
+                    return '';
+                }
+
+            });
     }
 }
